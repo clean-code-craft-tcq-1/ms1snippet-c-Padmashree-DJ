dd735f7008ab4f756512747ec4b48500b2ee5c61
ECHO is on <@apac.bosch.com>
2021-03-25 13:36:36 +0530

    Assignment-1

--- test-validationtool.cpp
+++ test-validationtool.cpp
@@ -3,25 +3,25 @@
 #include "test/catch.hpp"
 #include "sensor-validate.h"
 
-typedef double doubleType;
+
 typedef enum
 {
     SOCMaxDeltaValue = 0.05,
 	CurrentMaxDeltaValue =0.1
-}doubleType;
+}MaxJumpType;
     
 TEST_CASE("reports error when soc jumps abruptly") {
   double socReadings[] = {0.0, 0.01, 0.5, 0.51};
-  double SOCMaxDeltaValue=0.05;
+  // double SOCMaxDeltaValue=0.05;
   int numOfSocReadings = sizeof(socReadings) / sizeof(socReadings[0]);
-  REQUIRE(Validate_Sensor_Readings(socReadings, numOfSocReadings, SOCMaxDeltaValue) == 0);
+  REQUIRE(Validate_Sensor_Readings(socReadings, numOfSocReadings, (double)SOCMaxDeltaValue) == 0);
 }
 
 TEST_CASE("reports error when current jumps abruptly") {
   double currentReadings[] = {0.0, 0.02, 0.03, 0.33};
-  double CurrentMaxDeltaValue= 0.1;
+  // double CurrentMaxDeltaValue= 0.1;
   int numOfCurReadings = sizeof(currentReadings) / sizeof(currentReadings[0]);
-  REQUIRE(Validate_Sensor_Readings(currentReadings, numOfCurReadings, CurrentMaxDeltaValue) == 0);
+  REQUIRE(Validate_Sensor_Readings(currentReadings, numOfCurReadings, (double)CurrentMaxDeltaValue) == 0);
 }
 
 TEST_CASE("reports error when Readings are not entered") {
