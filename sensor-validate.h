1c8b21fe16ebbdefa97a0a8d936646404ef6a637
ECHO is on <@apac.bosch.com>
2021-03-25 13:44:20 +0530

    Assignment-1

--- sensor-validate.h
+++ sensor-validate.h
@@ -2,6 +2,12 @@
 
 #define NULL_PTR  ((void *)0)
 
+typedef struct
+{
+    double SOCMaxDeltaValue;
+	double CurrentMaxDeltaValue;
+}MaxJumpType;
+    
 int IsSensorInputBreached_Return_1(double* Senosor_Values);
 int IsValidSensorInputwithinRange_Return_0(double* Senosor_Values, int NumOfValues, double MaxDeltaValue);
 int Validate_Sensor_Readings(double* values, int numOfValues, double MaxDeltaValue);
--- test-validationtool.cpp
+++ test-validationtool.cpp
@@ -4,24 +4,22 @@
 #include "sensor-validate.h"
 
 
-typedef enum
-{
-    SOCMaxDeltaValue = 0.05,
-	CurrentMaxDeltaValue =0.1
-}MaxJumpType;
-    
+MaxJumpType MaxDeltaValues={
+	0.05,
+	0.1
+};
 TEST_CASE("reports error when soc jumps abruptly") {
   double socReadings[] = {0.0, 0.01, 0.5, 0.51};
   // double SOCMaxDeltaValue=0.05;
   int numOfSocReadings = sizeof(socReadings) / sizeof(socReadings[0]);
-  REQUIRE(Validate_Sensor_Readings(socReadings, numOfSocReadings, (double)SOCMaxDeltaValue) == 0);
+  REQUIRE(Validate_Sensor_Readings(socReadings, numOfSocReadings, MaxDeltaValues.SOCMaxDeltaValue) == 0);
 }
 
 TEST_CASE("reports error when current jumps abruptly") {
   double currentReadings[] = {0.0, 0.02, 0.03, 0.33};
   // double CurrentMaxDeltaValue= 0.1;
   int numOfCurReadings = sizeof(currentReadings) / sizeof(currentReadings[0]);
-  REQUIRE(Validate_Sensor_Readings(currentReadings, numOfCurReadings, (double)CurrentMaxDeltaValue) == 0);
+  REQUIRE(Validate_Sensor_Readings(currentReadings, numOfCurReadings, MaxDeltaValues.CurrentMaxDeltaValue) == 0);
 }
 
 TEST_CASE("reports error when Readings are not entered") {
